6d6f092257ac83a6c234f9d7198bb782c30b62f2
Danny Goldstein <dgold@berkeley.edu>
2021-06-10 14:05:12 -0400

    automatically convert camelCase to snake_case in pydantic

--- run.py
+++ run.py
@@ -38,7 +38,7 @@ class SweepRun(BaseModel):
     """
 
     name: Optional[str] = None
-    summary_metrics: dict = Field(default_factory=lambda: {})
+    summary_metrics: dict = Field(default_factory=lambda: {}, alias="summaryMetrics")
     history: List[dict] = Field(default_factory=lambda: [])
     config: dict = Field(default_factory=lambda: {})
     state: RunState = RunState.proposed
