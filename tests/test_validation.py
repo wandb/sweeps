972807288a3a65d6589c21663b1367a95959cbb7
Danny Goldstein <dgold@berkeley.edu>
2021-06-14 17:44:30 -0400

    improve coverage in validation tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,6 +1,10 @@
 import pytest
 from jsonschema import ValidationError
-from .. import next_run
+from .. import next_run, stop_runs
+from ..bayes_search import bayes_search_next_run
+from ..grid_search import grid_search_next_run
+from ..random_search import random_search_next_run
+from ..hyperband_stopping import hyperband_stop_runs
 
 
 @pytest.mark.parametrize("search_type", ["bayes", "grid", "random"])
@@ -15,6 +19,18 @@ def test_validation_disable(search_type):
 
     with pytest.raises(ValidationError):
         _ = next_run(invalid_schema, [], validate=True)
+
+    with pytest.raises(ValidationError):
+        _ = stop_runs(invalid_schema, [], validate=True)
+        _ = hyperband_stop_runs([], invalid_schema, validate=True)
+
+    with pytest.raises(ValidationError):
+        if search_type == "bayes":
+            _ = bayes_search_next_run([], invalid_schema, validate=True)
+        elif search_type == "grid":
+            _ = grid_search_next_run([], invalid_schema, validate=True)
+        elif search_type == "random":
+            _ = random_search_next_run(invalid_schema, validate=True)
 
     # check that no error is raised
     result = next_run(invalid_schema, [], validate=False)
