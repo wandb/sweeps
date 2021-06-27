ac2bdb789f42b266f0cc6128645ca982db3ff493
Danny Goldstein <dgold@berkeley.edu>
2021-06-26 21:17:51 -0700

    export fill_schema

--- config/__init__.py
+++ config/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """Sweep config interface."""
 from .cfg import SweepConfig, schema_violations_from_proposed_config
+from .schema import fill_schema
 
-__all__ = [
-    "SweepConfig",
-    "schema_violations_from_proposed_config",
-]
+__all__ = ["SweepConfig", "schema_violations_from_proposed_config", "fill_schema"]
