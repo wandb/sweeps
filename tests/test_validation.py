1c12a89ec1825b88541e06bd04ae7d17bead963d
Danny Goldstein <dgold@berkeley.edu>
2021-06-30 12:09:45 -0700

    add test

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,6 +1,6 @@
 import pytest
 from jsonschema import ValidationError
-from .. import next_run, stop_runs
+from .. import next_run, stop_runs, SweepConfig
 from ..bayes_search import bayes_search_next_run
 from ..grid_search import grid_search_next_run
 from ..random_search import random_search_next_run
@@ -35,3 +35,18 @@ def test_validation_disable(search_type):
     # check that no error is raised
     result = next_run(invalid_schema, [], validate=False)
     assert result is not None
+
+
+@pytest.mark.parametrize("controller_type", ["cloud", "local", "invalid"])
+def test_controller(controller_type):
+    schema = {
+        "controller": controller_type,
+        "method": "random",
+        "parameters": {"a": {"values": [1, 2, 3, 4]}},
+    }
+
+    if controller_type in ["cloud", "local"]:
+        assert SweepConfig(schema)["controller"] == controller_type
+    else:
+        with pytest.raises(ValidationError):
+            SweepConfig(schema)
