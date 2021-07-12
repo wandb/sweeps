05b7bad4aaa45610610f4316177fe60862146c0d
Danny Goldstein <dgold@berkeley.edu>
2021-07-12 12:43:15 -0700

    fix type checking

--- _types.py
+++ _types.py
@@ -1,15 +1,8 @@
-from typing import Union, TYPE_CHECKING, Any
+from typing import Union, Any
 
 import numpy as np
-from distutils.version import LooseVersion
 
 
 floating = Union[float, np.floating]
 integer = Union[int, np.integer]
-
-if TYPE_CHECKING and LooseVersion(np.version.version) < LooseVersion("1.20"):
-    ArrayLike: Any
-else:
-    from numpy import typing as npt
-
-    ArrayLike = npt.ArrayLike
+ArrayLike = Any
