d22ed9a572b44294beb683c4980939c79e2b0ca0
Danny Goldstein <dgold@berkeley.edu>
2021-08-18 16:18:15 -0700

    Revert "Revert "Vectorize sweeps so that it can be used as a target for NextNArgs in gorilla (#33)" (#37)"
    This reverts commit b0d1b4d3ab48a9c18e191910cb3c527008e60b00.

--- random_search.py
+++ random_search.py
@@ -2,24 +2,25 @@ from .config.cfg import SweepConfig
 from .run import SweepRun
 from .params import HyperParameterSet
 
-from typing import Union
+from typing import Union, List
 
 
-def random_search_next_run(
-    sweep_config: Union[dict, SweepConfig], validate: bool = False
-) -> SweepRun:
+def random_search_next_runs(
+    sweep_config: Union[dict, SweepConfig], validate: bool = False, n: int = 1
+) -> List[SweepRun]:
     """Suggest runs with Hyperparameters sampled randomly from specified distributions.
 
-    >>> suggestion = random_search_next_run({'method': 'random', 'parameters': {'a': {'min': 1., 'max': 2.}}})
+    >>> suggestions = random_search_next_runs({'method': 'random', 'parameters': {'a': {'min': 1., 'max': 2.}}})
 
     Args:
         sweep_config: The sweep's config.
         validate: Whether to validate `sweep_config` against the SweepConfig JSONschema.
            If true, will raise a Validation error if `sweep_config` does not conform to
            the schema. If false, will attempt to run the sweep with an unvalidated schema.
+        n: The number of runs to return
 
     Returns:
-        The suggested run.
+        The suggested runs.
     """
 
     # ensure that the sweepconfig is properly formatted
@@ -28,10 +29,13 @@ def random_search_next_run(
 
     if sweep_config["method"] != "random":
         raise ValueError("Invalid sweep configuration for random_search_next_run.")
-
     params = HyperParameterSet.from_config(sweep_config["parameters"])
 
-    for param in params:
-        param.value = param.sample()
+    retval = []
+    for _ in range(n):
+        for param in params:
+            param.value = param.sample()
+        run = SweepRun(config=params.to_config())
+        retval.append(run)
 
-    return SweepRun(config=params.to_config())
+    return retval
