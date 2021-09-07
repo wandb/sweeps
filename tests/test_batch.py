b79b81510459c19d5ebfbeed1894916581c949cd
Danny Goldstein <dgold@berkeley.edu>
2021-09-07 16:32:16 -0700

    add randomness in the seed generation for test_batch

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -4,21 +4,22 @@ import numpy as np
 from ..run import next_run, next_runs
 
 
-def reset_seed():
-    np.random.seed(12)
-    random.seed(13)
+def reset_seed(init_seed):
+    np.random.seed(init_seed)
+    random.seed(init_seed + 1)
 
 
 def test_batch_bayes_search_same_answer_as_single_search(
     sweep_config_bayes_search_2params_with_metric,
 ):
-    reset_seed()
+    init_seed = random.randrange(0, 2 ** 32 - 1)
+    reset_seed(init_seed)
     runs = []
     for _ in range(10):
         suggestion = next_run(sweep_config_bayes_search_2params_with_metric, runs)
         runs.append(suggestion)
 
-    reset_seed()
+    reset_seed(init_seed)
     suggestions = next_runs(sweep_config_bayes_search_2params_with_metric, [], n=10)
     assert suggestions == runs
 
@@ -38,21 +39,21 @@ def test_batch_grid_search_same_answer_as_single_search(
         assert s in runs
 
 
-def test_batch_random_search_same_answer_as_single_search(
-    sweep_config_bayes_search_2params_with_metric,
-):
+def test_batch_random_search_same_answer_as_single_search():
 
     config = {
         "method": "random",
         "parameters": {"v1": {"values": [1, 2, 3]}, "v2": {"values": [4, 5]}},
     }
 
-    reset_seed()
+    init_seed = random.randrange(0, 2 ** 32 - 1)
+    reset_seed(init_seed)
+
     runs = []
     for _ in range(10):
         suggestion = next_run(config, runs)
         runs.append(suggestion)
 
-    reset_seed()
+    reset_seed(init_seed)
     suggestions = next_runs(config, [], n=10)
     assert suggestions == runs
