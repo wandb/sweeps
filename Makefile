3b2760fe58ffc0752cde000156fc7865fb6c9630
Danny Goldstein <dgold@berkeley.edu>
2021-09-13 22:39:25 -0700

    add setup.py, split out requirements into two files, update makefile targets

--- Makefile
+++ Makefile
@@ -1,3 +1,30 @@
 lint:
 	pre-commit run --all-files
 	mypy --scripts-are-modules --ignore-missing-imports .
+
+dist: clean ## builds source and wheel package
+	python setup.py sdist bdist_wheel
+	ls -l dist
+
+clean: clean-build clean-pyc clean-test ## remove all build, test, coverage and Python artifacts
+
+clean-build: ## remove build artifacts
+	rm -fr build/
+	rm -fr dist/
+	rm -fr .eggs/
+	find . -name '*.egg-info' -exec rm -fr {} +
+	find . -name '*.egg' -exec rm -f {} +
+
+clean-test: ## remove test and coverage artifacts
+	rm -fr .tox/
+	rm -f .coverage
+	rm -fr htmlcov/
+
+clean-pyc: ## remove Python file artifacts
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . -name '__pycache__' -exec rm -fr {} +
+
+release: dist ## package and upload release
+	twine upload dist/*
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open("requirements.test.txt") as requirements_file:
+    test_requirements = requirements_file.read().splitlines()
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="sweeps",
+    version="0.0.1",
+    description="Weights and Biases Hyperparameter Sweeps Engine.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Weights & Biases",
+    author_email="support@wandb.com",
+    url="https://github.com/wandb/sweeps",
+    packages=["sweeps", "sweeps.config"],
+    package_dir={"sweeps": "."},
+    package_data={"sweeps": ["config/schema.json"]},
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    test_suite="tests",
+    tests_require=test_requirements,
+)
