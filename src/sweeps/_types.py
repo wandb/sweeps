ce1d0664c9321be028bd381db8c12a7664376a38
Hu Po <hugo.ponte@wandb.com>
2022-04-15 12:54:38 -0500

    ArrayLike type

--- src/sweeps/_types.py
+++ src/sweeps/_types.py
@@ -1,8 +1,8 @@
-from typing import Union, Any
+from typing import Any, List, Tuple, Union
 
 import numpy as np
 
 
 floating = Union[float, np.floating]
 integer = Union[int, np.integer]
-ArrayLike = Any
+ArrayLike = Union[List, Tuple, np.ndarray]
