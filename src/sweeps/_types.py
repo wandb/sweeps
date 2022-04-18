83bb1bc5f5b02a72bc76cbce584cdd626997d70f
Hu Po <hugo.ponte@wandb.com>
2022-04-18 08:21:17 -0500

    code improvement

--- src/sweeps/_types.py
+++ src/sweeps/_types.py
@@ -1,8 +1,10 @@
-from typing import Union, Any
+""" Custom types for sweeps, mainly for type-checking. """
+
+from typing import Any, List, Tuple, Union
 
 import numpy as np
 
 
 floating = Union[float, np.floating]
 integer = Union[int, np.integer]
-ArrayLike = Any
+ArrayLike = Union[List, Tuple, np.ndarray]
