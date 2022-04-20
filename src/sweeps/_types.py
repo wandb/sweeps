5e0efdfd71702863c45f7e57b1bf15b6084dddc9
Hu Po <hugo.ponte@wandb.com>
2022-04-20 11:26:09 -0500

    mypy types again

--- src/sweeps/_types.py
+++ src/sweeps/_types.py
@@ -1,12 +1,14 @@
 """ Custom types for sweeps, mainly for type-checking. """
 
-from typing import Any, List, Tuple, Union
+from typing import Any, Union
+
+# from typing import Any, List, Tuple, Union
 
 import numpy as np
 
 
 floating = Union[float, np.floating]
 integer = Union[int, np.integer]
-# TODO: Remove Any from here
-#   mypy is very picky when it comes to supported operand types
-ArrayLike = Union[Any, List, Tuple, np.ndarray]
+ArrayLike = Any
+# TODO: mypy is very picky when it comes to supported operand types
+# ArrayLike = Union[List, Tuple, np.ndarray]
