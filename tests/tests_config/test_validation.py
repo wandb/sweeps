af77a160c717d900fbd60e80f1b53050ecf1f758
Hu Po <hugo.ponte@wandb.com>
2022-04-27 15:58:02 -0500

    test typo

--- tests/tests_config/test_validation.py
+++ tests/tests_config/test_validation.py
@@ -169,7 +169,7 @@ def test_nested_run_parameter(search_type):
     }
     desired_run_config = {
         # User gets the original parameters as specified
-        "foo": {"value": 1},
+        "foo": {"value": 2},
         "a.b": {"value": 2},
         "a.c": {"value": 3},
         "a.d.e": {"value": 4},
