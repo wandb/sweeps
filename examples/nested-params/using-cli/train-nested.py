d76f6fa95990e55de59d42f4d94c5eafb7861104
Hu Po <hugo.ponte@wandb.com>
2022-05-09 11:48:44 -0500

    lr optimizer argparse unknown

--- examples/nested-params/using-cli/train-nested.py
+++ examples/nested-params/using-cli/train-nested.py
@@ -17,22 +17,21 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 parser.add_argument("--project", type=str, default=None, help="project")
 parser.add_argument("--epochs", type=int, default=5)
 parser.add_argument("--batch_size", type=int, default=16)
-parser.add_argument("--lr", type=float, default=0.01)
-
-
-def train_function(config):
-    # Do some fake taining
-    for _ in range(config["epochs"]):
-        # You can access nested properties in the config!
-        _batch_size = config["batch_size"]
-        _lr = config["optimizer"]["lr"]
-        print(f"Fake training with batch size {_batch_size} and lr {_lr}")
-        wandb.log({"loss": 0.01})
-        time.sleep(0.3)
-
+parser.add_argument("--optimizer", type=dict, default=None)
+# parser.add_argument("--lr", type=float, default=0.01)
+# parser.add_argument("--optimizer.lr", type=float, default=0.01)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     wandb.init(project=args.project, config=args)
-    train_function(wandb.config)
+    
+    # Do some fake taining
+    for _ in range(wandb.config["epochs"]):
+        # You can access nested properties in the config!
+        _batch_size = wandb.config["batch_size"]
+        _lr = wandb.config["optimizer"]["lr"]
+        print(f"Fake training with batch size {_batch_size} and lr {_lr}")
+        wandb.log({"loss": 0.01})
+        time.sleep(0.3)
+
     wandb.finish()
