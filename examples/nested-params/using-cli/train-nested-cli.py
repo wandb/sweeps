366cdb02e4a8c9cc604d2aa1c496f25990b025df
Hugo <8493427+hu-po@users.noreply.github.com>
2022-05-12 07:51:33 -0500

    [WB-9591] Docs/nested config examples (#76)
    * cli and py examples
    
    * formatting
    
    * bugfix
    
    * more bugfix
    
    * more param validation error
    
    * else not finally
    
    * lr optimizer argparse unknown
    
    * loss affected by param values
    
    * try with str argparse
    
    * formatting
    
    * rename to prevent script module mypy

---
+++ examples/nested-params/using-cli/train-nested-cli.py
@@ -0,0 +1,40 @@
+"""
+    Example using nested configs for sweeps and agents that
+    are defined via YAML and run from the CLI. To run this example:
+
+    > wandb sweep sweep-nested.yaml
+
+    Then use the sweep id to run the agent:
+
+    > wandb agent <USERNAME/PROJECTNAME/SWEEPID>
+"""
+import argparse
+import time
+
+import wandb
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--project", type=str, default=None, help="project")
+parser.add_argument("--epochs", type=int, default=5)
+parser.add_argument("--batch_size", type=int, default=16)
+parser.add_argument("--optimizer", type=str, default=None)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    wandb.init(project=args.project, config=args)
+
+    # Do some fake taining
+    for epoch in range(wandb.config["epochs"]):
+        # You can access nested properties in the config!
+        _batch_size = wandb.config["batch_size"]
+        _lr = wandb.config["optimizer"]["lr"]
+        print(f"Fake training with batch size {_batch_size} and lr {_lr}")
+        # Fake loss has following relationships:
+        # - goes down with each epoch
+        # - larger batch size makes it go down faster
+        # - larger learning rate makes it go down faster
+        _fake_loss = 1 - (epoch / wandb.config["epochs"]) * _lr * _batch_size
+        wandb.log({"loss": _fake_loss})
+        time.sleep(0.3)
+
+    wandb.finish()
