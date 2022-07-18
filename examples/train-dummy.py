9e14a6f7e81d149f26c78ea01babe7c79c4f6fa8
Hu Po <hugo.ponte@wandb.com>
2022-07-18 11:19:04 -0500

    use jobs in train-dummy

--- examples/train-dummy.py
+++ examples/train-dummy.py
@@ -6,6 +6,7 @@ import time
 import wandb
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--use_jobs", type=bool, default=False, help="Uses Jobs, an experimental feature of Launch")
 parser.add_argument("--project", type=str, default=None, help="project")
 parser.add_argument("--epochs", type=int, default=27, help="epochs")
 parser.add_argument("--variance", type=float, default=1, help="param1")
@@ -15,11 +16,18 @@ parser.add_argument("--direction", type=float, default=5, help="param2")
 parser.add_argument("--sleep", type=float, default=5, help="param2")
 args = parser.parse_args()
 
-run = wandb.init(project=args.project)
-# Force configuration update (should not be needed in the future)
-wandb.config.update({"dummy": 1})
 
-time.sleep(1)
+if args.use_jobs:
+    settings = wandb.Settings()
+    settings.update({"enable_job_creation": True})
+    run = wandb.init(project=args.project, settings=settings)
+    # This will trigger the creation of a Job artifact.
+    run.log_code()
+else:
+    run = wandb.init(project=args.project)
+    # Force configuration update (should not be needed in the future)
+    wandb.config.update({"dummy": 1})
+    time.sleep(1)
 
 # The wandb config object holds the latest hyperparameter values
 print(f"wandb.config: {wandb.config}")
@@ -32,3 +40,4 @@ for e, x in enumerate(range(args.epochs)):
     wandb.log({"loss_metric": metric})
     print("INFO:  %3d = %f" % (e, metric))
     time.sleep(args.sleep)
+
