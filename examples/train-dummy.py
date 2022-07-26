9302963ff882850b75457ab4d4f903a3952ad52e
Hu Po <hugo.ponte@wandb.com>
2022-07-26 11:58:04 -0500

    wandb finish required for jobs

--- examples/train-dummy.py
+++ examples/train-dummy.py
@@ -2,18 +2,19 @@
 import argparse
 import random
 import time
+import os
 
 import wandb
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--use_jobs", action='store_true', help="Uses Jobs, an experimental feature of Launch")
 parser.add_argument("--project", type=str, default=None, help="project")
-parser.add_argument("--epochs", type=int, default=27, help="epochs")
+parser.add_argument("--epochs", type=int, default=5, help="epochs")
 parser.add_argument("--variance", type=float, default=1, help="param1")
 parser.add_argument("--base", type=float, default=5, help="param2")
 parser.add_argument("--increment", type=float, default=5, help="param2")
 parser.add_argument("--direction", type=float, default=5, help="param2")
-parser.add_argument("--sleep", type=float, default=5, help="param2")
+parser.add_argument("--sleep", type=float, default=1, help="param2")
 args = parser.parse_args()
 
 
@@ -21,7 +22,7 @@ if args.use_jobs:
     settings = wandb.Settings()
     settings.update({"enable_job_creation": True})
     run = wandb.init(project=args.project, settings=settings)
-    # This will trigger the creation of a Job artifact.
+    # This will trigger the creation of aget Job artifact.
     run.log_code()
 else:
     run = wandb.init(project=args.project)
@@ -41,3 +42,5 @@ for e, x in enumerate(range(args.epochs)):
     print("INFO:  %3d = %f" % (e, metric))
     time.sleep(args.sleep)
 
+if args.use_jobs:
+    wandb.finish()
