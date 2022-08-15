0f7c1813a993bf378fbe4d916ced62262d22cefc
Hu Po <hugo.ponte@wandb.com>
2022-08-15 15:55:34 -0500

    Bump version: 0.1.1.dev1 → 0.1.1

--- src/sweeps/__init__.py
+++ src/sweeps/__init__.py
@@ -1,4 +1,4 @@
 from .config import SweepConfig, schema_violations_from_proposed_config  # noqa
 from .run import RunState, SweepRun, next_run, next_runs, stop_runs  # noqa
 
-__version__ = "0.1.1.dev1"
+__version__ = "0.1.1"
